eb9cfaccaa703585e4ff412026415428aa456566
= <=>
2021-11-26 14:26:57 +0700

    Update Test.java

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,6 +1,16 @@
 package main;
 
+import javax.swing.UIManager;
+
 public class Test {
+	static {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception e1) {
+			
+		}
+	}
+	
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
